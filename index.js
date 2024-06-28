46f1a16b403b0416c2d71c6b8134079e7ca0c77c
Pragalathan-cyber <152346883+Pragalathan-cyber@users.noreply.github.com>
2024-06-28 18:34:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const path = require('path')
 mongoose.set('strictQuery', true);
 
 const corsOptions = {
-  origin: ['https://dane-connect.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Enable credentials (cookies, authorization headers, etc.)
